55290d003788c306c53e6f5e203de14d8512a1c7
Fábio Reis <fabio.reis@qwasolucoes.com.br>
2021-11-03 14:58:31 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Olá mundo!"); // sem quebra de linha
+		System.out.println("Boa tarde"); // com quebra de linha
+	}
+
+}
